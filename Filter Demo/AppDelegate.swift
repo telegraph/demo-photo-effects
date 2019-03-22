d3b03c8fdb54ba627ca35552908ff4ee1d194f83
Michael Baldock <michael.baldock@telegraph.co.uk>
2019-03-22 11:00:17 +0000

    Posterize App Demo Project (with whitewalker)

---
+++ Filter Demo/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Filter Demo
+//
+//  Created by Michael Baldock on 21/03/2019.
+//  Copyright © 2019 Michael Baldock. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+
+}
+
